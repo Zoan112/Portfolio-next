dc065b751ed6332d131e1c4848356c42e0e6207f
Zoan112 <zohar.hadari@gmail.com>
2021-06-15 20:18:34 +0300

    mobile menu almost ready

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const mobileMenu = document.getElementsByClassName("mobileMenuDiv")[0]
+/*const mobileMenu = document.getElementsByClassName("mobileMenuDiv")[0]
+const mobileMenuList = document.getElementsByClassName("nav")[0]
 let menuOpen = false;
 
 console.log(mobileMenu)
@@ -11,10 +12,13 @@ mobileMenu.addEventListener('click', ()=>{
 mobileMenu.addEventListener('click', () => {
     if (!menuOpen){
     mobileMenu.classList.add("open");
+    mobileMenuList.classList.add("open");
     menuOpen = true
     }else{
         mobileMenu.classList.remove("open");
+        mobileMenuList.classList.remove("open");
         menuOpen = false;
     }
   });
 
+*/
