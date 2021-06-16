b7c850c6188e0858ac7b10f19d1c26930061a3d2
Zoan112 <zohar.hadari@gmail.com>
2021-06-16 14:45:11 +0300

    Added sent sucsess to contact form

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
+//Mobile Menu
 const mobileMenu = document.getElementsByClassName("mobileMenuDiv")[0]
 const mobileMenuList = document.getElementsByClassName("nav")[0]
 let menuOpen = false;
-
-
 
 mobileMenu.addEventListener('click', () => {
     if (!menuOpen){
@@ -16,3 +15,28 @@ mobileMenu.addEventListener('click', () => {
     }
   });
 
+//Contact form
+
+ 
+const submitBtn = document.getElementsByClassName("submitBtn")[0];
+const contactForm = document.getElementsByClassName("contactForm")[0]
+const msgStatus = document.getElementsByClassName("msgStatus")[0]
+
+//Submit contact form
+
+submitBtn.addEventListener("click", function(e){
+      e.preventDefault()
+
+
+      let myForm = document.getElementById('contactForm')
+      
+      console.log(myForm)
+      let formData = new FormData(myForm)
+      fetch('/', {
+        method: 'POST',
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: new URLSearchParams(formData).toString()
+      }).then(() => console.log('Form successfully submitted'),contactForm.style.display = "none", msgStatus.style.display = "flex").catch((error) =>
+        alert(error))
+
+})
